8911bd81ca6c127da3fa608f673ba9e6bae4ab2b
Hestie <hestie.seyfarth@waracle.com>
2024-01-17 12:15:53 +0000

    fix create database - load md with textloader

--- create_database.py
+++ create_database.py
@@ -1,7 +1,8 @@
-from langchain.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
 import os
 import shutil
@@ -19,13 +20,15 @@ def main():
 
 def generate_data_store():
     documents = load_documents()
-    # chunks = split_text(documents)
-    # save_to_chroma(chunks)
+    chunks = split_text(documents)
+    save_to_chroma(chunks)
 
 
 def load_documents():
-    loader = DirectoryLoader(DATA_PATH, glob="*.md")
+    # for PDF see https://python.langchain.com/docs/modules/data_connection/document_loaders/pdf
+    loader = DirectoryLoader(DATA_PATH, glob="**/*.md", loader_cls=TextLoader)
     documents = loader.load()
+    print(len(documents))
     return documents
 
 
